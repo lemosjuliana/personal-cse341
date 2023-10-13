4a333fced5f31c6084629678708305ff43fd385c
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-12 23:32:12 -0300

    L05 - Swagger error

--- controllers/user.js
+++ controllers/user.js
@@ -1,34 +1,50 @@
-const User = require('../models/user');
+const db = require('../models');
+const User = db.user;
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send({ message: 'Content can not be empty!' });
+    return;
+  }
 
-// GET all users
-const getAll = async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  
-  // POST a new user
-  const createUser = async (req, res) => {
-    const { username, email } = req.body;
-  
-    try {
-      const newUser = new User({
-        username,
-        email,
+  const user = new User(req.body);
+  user
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the user.'
       });
-  
-      await newUser.save();
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+    });
+};
 
-  module.exports = { getAll, createUser };
+exports.getAll = (req, res) => {
+  User.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving users.'
+      });
+    });
+};
 
+exports.getUser = (req, res) => {
+  const username = req.params.username;
+  User.find({ username: username })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving users.'
+      });
+    });
+};
   
 // // Create a PUT route for updating a contact that 
 // // returns a 204 status
