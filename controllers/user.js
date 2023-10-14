b139e774f9cce4f7d490d8cd05f0d93e41de5d1a
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-14 01:37:28 -0300

    L05 - DELETE AND POST routes

--- controllers/user.js
+++ controllers/user.js
@@ -53,17 +53,8 @@ const createUser = async (req, res) => {
 
 // I still have to create the PUT and DELETE functions (they are not required for Lesson 05)
 
-const updateUser = async (req, res) => {
-  // Function will be created
-};
-
-const deleteUser = async (req, res) => {
-  // Function will be created
-};
-
-module.exports = { getAllUsers, getSingleUser, createUser, updateUser, deleteUser };
+module.exports = { getAllUsers, getSingleUser, createUser };
 
 
-// module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
-
+// module.exports = { getAllUsers, getSingleUser, createUser, updateUser, deleteUser };
   
