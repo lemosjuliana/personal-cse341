57153f8b5763729c8bf9668f2d89e476c9932ea1
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-21 09:19:10 -0300

    L06 - fixing update user and get single user

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,29 +1,29 @@
 const validator = require('../helpers/validate');
 
 const saveUser = (req, res, next) => {
-    const validationRule = {
-        username: 'required|string',
-        species: 'required|string',
-        breed: 'string', // Allow it to be a string, but not required
-        color: 'string', // Allow it to be a string, but not required
-        gender: 'string', // Allow it to be a string, but not required
-        age: 'integer|min:0', // Assuming age should be a positive integer or zero
-        owner: 'required|object', // Assuming owner is required and an object
-        'owner.ownerName': 'required|string',
-        'owner.ownerEmail': 'required|email',
-        'owner.ownerPhoneNumber': 'required|string|regex:^[0-9]{3}-[0-9]{3}-[0-9]{4}$',
-        'owner.ownerAddress': 'required|string',
-        vaccines: 'required|array',
-        'vaccines.*.vaccineType': 'required|string',
-        'vaccines.*.date': 'required|date',
-        'vaccines.*.secondDose': 'required|boolean',
-        vet: 'required|object', // Assuming vet is required and an object
-        'vet.vetName': 'required|string',
-        'vet.vetSpecialization': 'array', // Assuming vetSpecialization is an array
-        'vet.vetSpecialization.*': 'string', // Assuming each item in vetSpecialization is a string
-        'vet.vetEmail': 'required|email',
-        'vet.vetPhoneNumber': 'required|string|regex:^[0-9]{3}-[0-9]{3}-[0-9]{4}$'
-      };
+  const validationRule = {
+    username: 'required|string',
+    species: 'required|string',
+    breed: 'sometimes|string',
+    color: 'sometimes|string',
+    gender: 'sometimes|string',
+    age: 'integer|min:0',
+    owner: 'required|object',
+    'owner.ownerName': 'required|string',
+    'owner.ownerEmail': 'required|email',
+    'owner.ownerPhoneNumber': 'required|string|regex:^[0-9]{3}-[0-9]{3}-[0-9]{4}$',
+    'owner.ownerAddress': 'required|string',
+    vaccines: 'required|array',
+    'vaccines.*.vaccineType': 'required|string',
+    'vaccines.*.date': 'required|date',
+    'vaccines.*.secondDose': 'required|boolean',
+    vet: 'required|object',
+    'vet.vetName': 'required|string',
+    'vet.vetSpecialization': 'array',
+    'vet.vetSpecialization.*': 'string',
+    'vet.vetEmail': 'required|email',
+    'vet.vetPhoneNumber': 'required|string|regex:^[0-9]{3}-[0-9]{3}-[0-9]{4}$'
+  };
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) {
       res.status(400).send({
@@ -37,10 +37,11 @@ const saveUser = (req, res, next) => {
   });
 };
 
+
 const saveReview = (req, res, next) => {
   const validationRule = {
     rating: 'required|integer|between:1,5',
-    comment: 'required|string|max:200', 
+    comment: 'required|string|max:200',
   };
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) {
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const usersController = require('../controllers/user');
 const validation = require('../middleware/validate');
 
 router.get('/', usersController.getAllUsers);
+router.get('/:id', usersController.getSingleUser);
 router.post('/', validation.saveUser, usersController.createUser);
 router.put('/:id', validation.saveUser, usersController.updateUser);
 // router.get('/:username', usersController.getSingleUser);  
