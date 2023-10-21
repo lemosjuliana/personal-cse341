13748e29d5084bcb96d59db5fee2146f6af189ed
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-21 08:49:29 -0300

    L06 - fixing routes - validation

--- routes/review.js
+++ routes/review.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const reviewController = require('../controllers/review');
 const validation = require('../middleware/validate');
 
-router.get('/', validation.saveReview, reviewController.getAllReviews);
-router.get('/:id', validation.saveReview, reviewController.getSingleReview);
+router.get('/', reviewController.getAllReviews);
+router.get('/:id', reviewController.getSingleReview);
 router.post('/', validation.saveReview, reviewController.createReview);
 router.put('/:id', validation.saveReview, reviewController.updateReview);
-router.delete('/:id', validation.saveReview,  reviewController.deleteReview);
+router.delete('/:id',  reviewController.deleteReview);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const usersController = require('../controllers/user');
 const validation = require('../middleware/validate');
 
-router.get('/', validation.saveUser, usersController.getAllUsers);
+router.get('/', usersController.getAllUsers);
 router.post('/', validation.saveUser, usersController.createUser);
 router.put('/:id', validation.saveUser, usersController.updateUser);
 // router.get('/:username', usersController.getSingleUser);  
-router.put('/:id', validation.saveUser, usersController.updateUser);
-router.delete('/:id', validation.saveUser, usersController.deleteUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
