88679ebdee67e71c049299c9101b63c892af9bc8
lemosjuliana <lemos.julianajesus@gmail.com>
2023-10-28 06:41:12 -0300

    L07 - Adding to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/user', require('./user'));
 router.use('/review', require('./review'));
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+router.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+  
+  
 
 module.exports = router;
 
